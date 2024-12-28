ecef3d9e26cb32d1c9e4fcbb0ef245cff355085a
422.io <iamwinner422@gmail.com>
2024-12-28 16:01:03 +0000

    Setuping NestConfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     EventEmitterModule.forRoot({
         maxListeners: 30
+    }),
+    ConfigModule.forRoot({
+        isGlobal: true
     })
   ],
   controllers: [AppController],
