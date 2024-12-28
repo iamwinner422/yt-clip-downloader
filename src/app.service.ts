011957facb6112961344f718647697ffbb2bed8e
422.io <iamwinner422@gmail.com>
2024-12-28 12:08:27 +0000

    installing dependencies

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+    getHello(): string {
+        return 'Hello World!';
+    }
+
+
+    async getInfo(videoURL: string) {
+        if(!videoURL) throw new BadRequestException('Video URL is required');
+    }
 }
