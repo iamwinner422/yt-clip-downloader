9d0cf79def84295fc2cc390b9b587ee38dcddf98
422.io <iamwinner422@gmail.com>
2024-12-28 11:25:22 +0000

    implémenting Swagger API Definition

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle('YT Clip Downloader')
+        .setDescription('Youtube Clip Downloader allows you to download specific video clips from YouTube by providing a video URL, start time, and clip duration.')
+        .setVersion('1.0')
+        .build();
+
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('', app, documentFactory);
+    
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
